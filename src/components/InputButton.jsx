0ada433fe059203b45f4406864af930e5001fd75
Matheus Amorim <rodriguesamorim0@gmail.com>
2025-04-20 11:17:52 -0300

    criação do componente para o card final e ajuste do transition dos botões

--- src/components/InputButton.jsx
+++ src/components/InputButton.jsx
@@ -7,6 +7,6 @@ export function InputButton ({ value,setNotaAvaliacao }) {
         <input type="button" value={value} className="bg-dark-blue w-10.5 h-10.5 
         rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey 
         focus:text-white sm:w-13 sm:h-13 hover:bg-orange hover:text-white 
-        hover:cursor-pointer" onClick={() => handleMudarNotaAvalicao(value)}/>
+        hover:cursor-pointer duration-500 font-bold" onClick={() => handleMudarNotaAvalicao(value)}/>
     )
 }
